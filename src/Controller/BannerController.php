75fe0a216e95f0cd63257aad9e1b9088f0e02e0e
raj shah <shahraj121997@gmail.com>
2021-05-03 14:21:58 -0400

    :art: Update : WIldCard in Routes added

--- src/Controller/BannerController.php
+++ src/Controller/BannerController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 
 
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
 
 class BannerController
 {
@@ -12,5 +13,28 @@ class BannerController
  {
      return new Response('Banner first Controller Created',200);
  }
+
+    /**
+     * @Route("/annotationRoute")
+     */
+    public function annotationRoute(): Response
+    {
+        return new Response('Annotation Based Route Directly From Controller',200);
+    }
+
+
+    // In Routes If we pass {wildString} then that value can be anything .
+    // That can also be accessed via string $courseID in Controller Functions
+    /**
+     * @Route("/grades/{courseID}")
+     */
+    public function grades(string $courseID): Response
+    {
+        // sprintf('value "%s"',$variableName) Make Sure starting is '';
+
+        return new Response(sprintf('This Route will give grades for subject : %s ',$courseID),200);
+    }
+
+
 
 }
