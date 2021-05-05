8acf029dc5b2e9a74126582adc0e39fd7df5cc41
raj shah <shahraj121997@gmail.com>
2021-05-05 03:28:04 -0400

    :art: Update : Code Refector

--- src/Controller/BannerController.php
+++ src/Controller/BannerController.php
@@ -11,34 +11,34 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class BannerController extends AbstractController
 {
- public function heropage(): Response
- {
-     //return new Response('Banner first Controller Created',200);
+    public function heropage(): Response
+    {
+        //return new Response('Banner first Controller Created',200);
 
-     // render method is inherited from AbstractController
-
-
-     // This Creates the Object of Type studentInfo
-     // Access Variables using get methods
-
-     // See AutoLoader is not used like PHP-Starter-Kit Project instead we have use
-     // use \App\studentInfo which Points to the model file
-     $studentInfoObj=new studentInfo('Raj Shah','rshah62@unc.edu',8012035638);
+        // render method is inherited from AbstractController
 
 
-     return $this->render('heropage/hero.html.twig',[
-         "Name"=>$studentInfoObj->getName(),
-         "UID"=>$studentInfoObj->getUID(),
-         "EmailID"=>$studentInfoObj->getEmailID()
-     ]);
- }
+        // This Creates the Object of Type studentInfo
+        // Access Variables using get methods
+
+        // See AutoLoader is not used like PHP-Starter-Kit Project instead we have use
+        // use \App\studentInfo which Points to the model file
+        $studentInfoObj = new studentInfo('Raj Shah', 'rshah62@unc.edu', 8012035638);
+
+
+        return $this->render('heropage/hero.html.twig', [
+            "Name" => $studentInfoObj->getName(),
+            "UID" => $studentInfoObj->getUID(),
+            "EmailID" => $studentInfoObj->getEmailID()
+        ]);
+    }
 
     /**
      * @Route(path="/annotationRoute",name="app_annotationRoute")
      */
     public function annotationRoute(): Response
     {
-        return new Response('Annotation Based Route Directly From Controller',200);
+        return new Response('Annotation Based Route Directly From Controller', 200);
     }
 
 
@@ -51,9 +51,8 @@ class BannerController extends AbstractController
     {
         // sprintf('value "%s"',$variableName) Make Sure starting is '';
 
-        return new Response(sprintf('This Route will give grades for subject : %s ',$courseID),200);
+        return new Response(sprintf('This Route will give grades for subject : %s ', $courseID), 200);
     }
-
 
 
 }
