1edd9946690a25e8fc66386e3faaf98249c2e255
raj shah <shahraj121997@gmail.com>
2021-05-05 02:12:07 -0400

    :construction: New : Grade.php Created with Methods

---
+++ src/Model/Grade.php
@@ -0,0 +1,44 @@
+<?php
+
+
+namespace App\Model;
+
+
+class Grade
+{
+    /**
+     * @var
+     */
+    private $percentage;
+    private $subjectName;
+
+
+    /**
+     * Grade constructor.
+     * @param $pecentage
+     * @param $subjectName
+     */
+    public function __construct($percentage, $subjectName)
+    {
+        $this->percentage = $percentage;
+        $this->subjectName = $subjectName;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getPercentage()
+    {
+        return $this->percentage;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getSubjectName()
+    {
+        return $this->subjectName;
+    }
+
+
+}
