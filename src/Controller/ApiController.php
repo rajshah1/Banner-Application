f9eee05157742e38890356d6802aa279b4d7dcb5
Raj Shah <rshah62@uncc.edu>
2021-05-23 01:09:48 -0400

    :sparkles: New :JSON Response Handler

--- src/Controller/ApiController.php
+++ src/Controller/ApiController.php
@@ -6,20 +6,63 @@ namespace App\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Cookie;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class ApiController
+ * @package App\Controller
+ */
 class ApiController extends AbstractController
 {
     /**
      * @Route(path="/api",name="api_entrypoint",condition="")
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @return \Symfony\Component\HttpFoundation\Response
+     * @group api
+     */
+    public function publicAPIEntry(Request $request): Response
+    {
+        $request->headers->set('testKeyPass', 'api Entry Point');
+        //dd($request->request->get('ApiPublicSecret'));
+        //dd($request);
+        $response = new Response('This is ENTRY Endpoint for Public API');
+        $response->headers->set('Test', 'JWT TOKEN');
+        //$response->setContent('text/html');
+        $response->setStatusCode(Response::HTTP_OK);
+        // Set Type based on request so imcompablity is ignored.
+        $response->prepare($request);
+        // Set Cookie By this way
+        $response->headers->setCookie(Cookie::create('Cokkie-API-Secret', '10018', 30000));
+        // Redirect USER TO other Page  From Controller
+        // $response = new RedirectResponse('http://example.com/');
+        return $response;
+    }
+
+
+    /**
+     * @Route(name="api_jsonResp",path="/api/json",methods={"POST","GET"})
+     * @group api
      * @return \Symfony\Component\HttpFoundation\Response
      */
-    public function publicAPIEntry(Request $request):Response{
-        $request->headers->set('testKeyPass','api Entry Point');
-        dd($request->request->get('ApiPublicSecret'));
-        //dd($request);
-        return new Response('This is ENTRY Endpoint for Public API',Response::HTTP_OK,['Test'=>'JWT TOKEN']);
+    public function jsonResponseAPI():Response
+    {
+        $response = new Response();
+        $response->setContent(json_encode([
+            'name' => 'Raj Shah',
+            'age' => 23
+        ])??null);
+        $response->headers->set('Content-Type', 'application/json');
+        $jsonResponse=new JsonResponse();
+        $jsonResponse->setContent(json_encode([
+            'test'=>'1'
+        ]));
+
+        return $response;
     }
+
+
 }
