408a264d1fb91d75d3fa3debe2b93245ef67ff4d
Raj Shah <rshah62@uncc.edu>
2021-07-01 20:32:33 -0400

    :arrow_up: Yarn Upgrade and Popper.js added

--- assets/app.js
+++ assets/app.js
@@ -7,6 +7,7 @@
 
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
-
+const $=require('jquery');
+require('bootstrap');
 // start the Stimulus application
 import './bootstrap';
